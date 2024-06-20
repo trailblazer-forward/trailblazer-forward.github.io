45fb40f0454b363b40730f1ae12fc033a06f9520
trailblazer-forward <trailblazer.forward@outlook.com>
2024-06-20 22:08:29 +0800

    chang helloworld.cpp

--- test-wasm/helloworld.cpp
+++ test-wasm/helloworld.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <format>
 #include <emscripten.h>
 using std::cout;
+using std::format;
 
 extern "C" void EMSCRIPTEN_KEEPALIVE say_hello()
 {
     cout << "Hello World!\n";
+    cout << format("{}\n", __cplusplus);
 }
